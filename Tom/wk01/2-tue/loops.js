869f41c9c0e32ad911057a418b6cb96228fceb50
tuckered <tom.a.waters@outlook.com>
2018-06-20 15:37:42 +1000

    completed variables and loops homework

---
+++ Tom/wk01/2-tue/loops.js
@@ -0,0 +1,93 @@
+// // The odd/even Reporter
+// for (var i = 1; i <= 20; i++) {
+    
+//     if (i % 2 === 0) {
+//     console.log(i + ' is even');
+//     } else {
+//         console.log(i + ' is odd');
+//     }
+// };
+
+// //Multiplication Tables
+
+// For loop that iterates from 0 - 10. Each loop multiplies by 9. 
+// for (var i = 0; i <= 10; i++)
+
+//     if (i) {
+//         console.log(i + ' x 9 = ' + (i * 9));
+//     };
+
+//Loop that iterates each table from 1 to 100. 
+// for (var i = 1; i <= 10; i++) {
+
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 1 = ' + (i * 1));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 2 = ' + (i * 2));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 3 = ' + (i * 3));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 4 = ' + (i * 4));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 5 = ' + (i * 5));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 6 = ' + (i * 6));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 7 = ' + (i * 7));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 8 = ' + (i * 8));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 9 = ' + (i * 9));
+//     }
+//     for (var i = 1; i <= 10; i++) {
+//         console.log(i + ' x 10 = ' + (i * 10));
+//     }
+//  };
+
+
+// //Nested for loop that dispalys the times table grid for each number up to 10. 
+// for (var i = 1; i <= 10; i++) {
+//     //
+//     for (var j = 1; j <= 10; j += 1) {
+//         console.log(i + ' x ' + j + ' = ' + j * i);
+//     }
+// };
+
+
+// Your Top Choices
+
+var colours = ['blue', 'purple', 'red', 'orange', 'pink'];
+var suffix = ['st', 'nd', 'rd', 'th']
+
+for (var i = 0; i < colours.length; i++) {
+    console.log('my # ' + (i+1) +  suffix[i] + ' choice is ' + colours[i]);
+}
+
+
+// console.log('My #1 choice is ' + favColours[0]);
+// console.log('My #2 choice is ' + favColours[1]);
+// console.log('My #3 choice is ' + favColours[2]);
+// console.log('My #4 choice is ' + favColours[3]);
+
+
+// var favColours = ['blue', 'yellow', 'green', 'orange'];
+
+// console.log('My 1st choice is ' + favColours[0]);
+// console.log('My 2nd choice is ' + favColours[1]);
+// console.log('My 3rd choice is ' + favColours[2]);
+// console.log('My 4th choice is ' + favColours[3]);
+
+// var favColours = ['blue', 'purple', 'green', 'orange'];
+
+// if (favColours = [0]) {
+//     console.log('My 1st choice is ' + favColours[0]);
+// };
+
