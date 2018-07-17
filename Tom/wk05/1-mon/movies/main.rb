4d7542ef6e84f09fff8ee29c9318aa99af679c90
tuckered <tom.a.waters@outlook.com>
2018-07-17 18:35:30 +1000

    initial commit

--- Tom/wk05/1-mon/movies/main.rb
+++ Tom/wk05/1-mon/movies/main.rb
@@ -9,10 +9,15 @@ get '/' do
   erb :index
 end
 
+
+get '/movie_search' do 
+  @search = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&s=' + params[:movie_name])
+  erb :search
+end
+
+
 get '/movie' do 
-  if params[:movie_name]
-    @movie = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&t=' + params[:movie_name])
-  end
+  @movie = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&t=' + params[:movie_name])
   erb :movies
 end
 
