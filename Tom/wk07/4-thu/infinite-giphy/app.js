eb7611ce0fd9967f234f181e92cac8d4ca59816f
tuckered <tom.a.waters@outlook.com>
2018-08-05 14:20:10 +1000

    giphy homework

--- Tom/wk07/4-thu/infinite-giphy/app.js
+++ Tom/wk07/4-thu/infinite-giphy/app.js
@@ -1,12 +1,10 @@
-
-
 var buttonClick = document.querySelector('.search_Btn')
 buttonClick.addEventListener('click', function(event) {
   event.preventDefault()
 
   var gifSearch = document.querySelector('.giphySearch').value
 
-  var searchRequest = $.get("http://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=VJOTaTGkypipTfFX0RT7zTXHhJII6NUw&limit=0")
+  var searchRequest = $.get("http://api.giphy.com/v1/gifs/search?q=" + gifSearch + "&api_key=VJOTaTGkypipTfFX0RT7zTXHhJII6NUw&search=100")
   
   searchRequest.done(function(response) { 
 
@@ -15,35 +13,61 @@ buttonClick.addEventListener('click', function(event) {
       var resultItem = document.querySelector('.results')
       var newImg = document.createElement('img')
       var mp4Img = response.data[i].images.downsized_large.url
+      console.log(i)
       newImg.src = mp4Img
       newImg.append(mp4Img)
       resultItem.append(newImg)
     }
-
   })
-
-  var num = 10
 
   $(window).scroll(function() {
     if($(window).scrollTop() == $(document).height() - $(window).height()) {
       searchRequest.done(function(response) { 
-        var j = 20
+        var j = 21
         for (i = 10; i < j; i++) {
-          j++
-          var resultItem = document.querySelector('.results')
-          var newImg = document.createElement('img')
-          var mp4Img = response.data[i].images.downsized_large.url
+          console.log(i)
+          resultItem = document.querySelector('.results')
+          newImg = document.createElement('img')
+          mp4Img = response.data[i].images.downsized_large.url
           newImg.src = mp4Img
           newImg.append(mp4Img)
           resultItem.append(newImg)
+          if (i % 10 === 0) {
+            console.log('true')
+            j= j + 10
+          }
         }
       })
     }
-  });
+  })
 })
 
-      
+
+// var j = 50
+// for (i = 10; i < j; i++) {
+//   if (i % 10 === 0) {
+//     j = j + 10
+//   }
+// }
+   
 
 
+// for (i = 10; i < j; i++) {
+//   console.log('i='+i)
+// 	if (i % 10 === 0) {
+//     console.log('true')
+//     j= j + 10
+//   }
+// }
+
+// for (j = 0; j < 0; j++) {
+//   for (var i = 1; i < j; i++) {
+// if (i % 10 === 0) {
+// j = j + 10
+//   }
+//   console.log('i='+i)
+//   }
+// console.log('j='+j)
+// }
 
 // giphy api : VJOTaTGkypipTfFX0RT7zTXHhJII6NUw
