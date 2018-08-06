871e2241b6d9ee69dff3e11ae51c55ebf4e10a70
Tom <thomasalanwaters@gmail.com>
2018-08-06 19:48:04 +1000

    commit 3 - squares

--- Tom/wk08/1-mon/pix_art/app.js
+++ Tom/wk08/1-mon/pix_art/app.js
@@ -2,11 +2,23 @@ var searchForm = $('.search-form')
 var inputBox = $(".color-select")
 var setColor = $(".set-color-btn")
 var brushBox = $(".brush-box")
+var pixelWall = $(".pixels")
 
 function handleSubmit(event) {
   event.preventDefault()
   var color = inputBox.val()
   $(".brush-box").css( "background-color", color)
+  
 }
+// make the element first, do what you want to it and then append it
+
+
+for (var i = 0; i < 1000; i ++){
+  var pixels = $('.pixels').append( "<div></div>" )
+  $(pixels).children().addClass('square')
+  
+  console.log(i)
+}
+
 
 searchForm.on('click', handleSubmit)
